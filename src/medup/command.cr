c7fa5aed6507a06a60178300cc7567c0ba4ab2f3
Michael Nikitochkin <miry.sof@gmail.com>
2019-12-04 22:44:30 +0100

    Add option to change default destination folder

--- src/medup/command.cr
+++ src/medup/command.cr
@@ -5,11 +5,13 @@ module Medup
     def self.run
       token = ENV.fetch("MEDIUM_TOKEN", "")
       user = nil
+      dist = ::Medup::Tool::DIST_PATH
       exit = false
 
       OptionParser.parse do |parser|
         parser.banner = "Usage: medup [arguments]"
         parser.on("-u USER", "--user=USER", "Medium author username. E.g: miry") { |u| user = u }
+        parser.on("-d DIRECTORY", "--directory=DIRECTORY", "Path to local directory where articles should be dumped. Default: ./posts") { |d| dist = d }
         parser.on("-h", "--help", "Show this help") { puts parser; exit = true }
         parser.on("-v", "--version", "Print current version") { puts ::Medup::VERSION; exit = true }
 
@@ -20,7 +22,7 @@ module Medup
 
       return if exit
 
-      tool = ::Medup::Tool.new(token, user)
+      tool = ::Medup::Tool.new(token: token, user: user, dist: dist)
       tool.backup
       tool.close
     end
